be9b23e38d3a92260c31680dd97c3e724d29aae8
Rich <applerich0306@gmail.com>
2024-11-14 23:45:28 +0200

    change eslint config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,8 +7,19 @@
 // export default nextConfig;
 
 
-// next.config.mjs
-export default {
-  reactStrictMode: true,
-  // Add other configurations here
-};
+// // next.config.mjs
+// export default {
+//   reactStrictMode: true,
+//   // Add other configurations here
+// };
+
+
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
+
+export default nextConfig
